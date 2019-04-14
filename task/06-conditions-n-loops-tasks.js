c46d1e63a4b40a568f416dd8d953ab777ac7d9de
yuliyasamsonova <yuliyasamsonova01@gmail.com>
2019-04-14 22:21:31 +0300

    did brackets

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -351,8 +351,40 @@ function getDigitalRoot(num) {
  *   '{[(<{[]}>)]}' = true 
  */
 function isBracketsBalanced(str) {
-    throw new Error('Not implemented');
+    var compare = [];
+    var a = false;
+    var sameBrackets = 0;
+    var bracketsConfig = [['(', ')'], ['[', ']'], ['{', '}'], ['<', '>']]
+    debugger;
+    for (var i = 0; i < str.length; i++) {
+        for (var j = 0; j < bracketsConfig.length; j++) {
+            for (var n = 0; n <= 1; n++) {
+                if (str[i] === bracketsConfig[j][n] && str[i] !== '|') {
+
+                    if (compare.length !== 0) {
+                        if (compare[compare.length - 1][0] === j && compare[compare.length - 1][1] === n - 1) {
+                            compare.pop();
+                        }
+                        else {
+                            compare.push([j, n]);
+                        }
+                    }
+                    else {
+                        compare.push([j, n]);
+                    }
+                }
+                if (str[i] === '|') {
+                    sameBrackets++;
+                }
+            }
+        }
+    }
+    if (compare.length === 0 && sameBrackets % 2 === 0) {
+        a = true;
+    }
+    return a;
 }
+
 
 
 /**
